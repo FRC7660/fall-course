5af5494142f2ec041f886b707dd647d6a61818f9
erclee2000 <98067054+erclee2000@users.noreply.github.com>
2022-11-05 13:17:08 -0400

    solutions to challenges 3 and 4 in "methods"

---
+++ assignment-06/solutions/MethodsChallenge3Sol.java
@@ -0,0 +1,36 @@
+/*
+For this challenge you will implement a public static method called sumOfDig
+Method input: an integer
+Method returns: the sum of all of the digits of the integer
+
+Example
+Input: 23224
+Returns: 13
+2 + 3 + 2 + 2 + 4 = 13
+
+Example
+Input: 764
+Returns: 17
+7 + 6 + 4 = 17
+*/
+public class MethodsChallenge3Sol {
+    public static int sumOfDig(int var) {
+        int sum = 0;
+        while(var > 0){
+            sum = sum + var%10;
+            var = var/10;
+        }
+        return sum; // return the resultant sum
+    }
+
+    public static void main(String[] args) {
+        System.out.println("input is 23224; the sum is " + sumOfDig(23224));
+        System.out.println("input is 0; the sum is " + sumOfDig(0));
+        System.out.println("input is 764234234; the sum is " + sumOfDig(764234234));
+        /* your terminal output should look like this:
+        input is 23224; the sum is 13
+        input is 0; the sum is 0
+        input is 764234234; the sum is 35
+        */
+    }
+}
